1e852aa13dbf9b0844240d4b781a50fdeecfc66d
LennoxHB <lennoxhwaru@hotmail.com>
2021-05-19 11:55:07 +1200

    Added a grid generator

--- andromeda.cs
+++ andromeda.cs
@@ -4,9 +4,18 @@ namespace project_andromeda
 {
     class andromeda
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello Space!");
+            string temp;
+            int input;
+            Random rand = new Random();
+            //Generates a two dimensional grid. NOTE: If you change the array size, make sure x is equal to y.
+            int[] xaxis = new int[5], yaxis = new int[5];
+            for (int i = 0; i < xaxis.Length; i++)
+            {
+                xaxis[i] = i;
+                yaxis[i] = i;
+            }
         }
     }
 }
