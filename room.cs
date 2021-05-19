0c544703e9224c8ddf14a8cc1170fae65fb138e6
LennoxHB <lennoxhwaru@hotmail.com>
2021-05-19 12:41:56 +1200

    Add room files

---
+++ room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace project_andromeda
+{
+    
+    public class Room
+    {
+        // This method loads a room into the game
+        public Room(int[] player)
+        {
+            int roomx, roomy;
+
+            roomx = player[0];
+            roomy = player[1];
+
+
+        }
+    }
+}
