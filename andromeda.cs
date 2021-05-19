88f0af534e27b7cbd336bb85c81547a138f9e710
GEVIS1 <GEVIS1@student.op.ac.nz>
2021-05-20 00:35:54 +1200

    Edit room.cs to read from file. Edit andromeda.cs

--- andromeda.cs
+++ andromeda.cs
@@ -6,96 +6,104 @@ namespace project_andromeda
     {
         static void Main()
         {
-            string temp;
-            int input;
-            Random rand = new Random();
-            //Generates a two dimensional grid. NOTE: If you change the array size, make sure x is equal to y.
-            int[] xaxis = new int[5], yaxis = new int[5];
-
-            // Populate array coordinates
-            for (int i = 0; i < xaxis.Length; i++)
-            {
-                xaxis[i] = i;
-                yaxis[i] = i;
-            }
-
-            //Player position, 0 = x axis, 1 = y axis, starting position is x2 y2.
             int[] player = new int[2];
             player[0] = 2;
             player[1] = 2;
 
+            Room.Read(player);
 
-            //Takes a user input to move player position
-            /*
-             * This will eventually need to take input as `<verb> <noun>` so you can actually interact with the environment.
-             * E.G. `use hammer`
-             * And if we have time you would have a context you want to do previous commands in too. So you could input `nail` after
-             * to instruct the hammer to be used on the nail.
-             * 
-             * */
+            // Very debug
+
+            //string temp;
+            //int input;
+            //Random rand = new Random();
+            ////Generates a two dimensional grid. NOTE: If you change the array size, make sure x is equal to y.
+            //int[] xaxis = new int[5], yaxis = new int[5];
+
+            //// Populate array coordinates
+            //for (int i = 0; i < xaxis.Length; i++)
+            //{
+            //    xaxis[i] = i;
+            //    yaxis[i] = i;
+            //}
+
+            ////Player position, 0 = x axis, 1 = y axis, starting position is x2 y2.
+            //int[] player = new int[2];
+            //player[0] = 2;
+            //player[1] = 2;
 
 
-            // Main game loop
-            /*
-             * Eventually this should maybe read a room text file? E.G. 22.room would be loaded at the start since player is at 
-             * position[0] = 2;
-             * position[1] = 2;
-             * 
-             * This room file would have information about what can be done in the room and the contextual information about what is in the room
-             * 
-             */
+            ////Takes a user input to move player position
+            ///*
+            // * This will eventually need to take input as `<verb> <noun>` so you can actually interact with the environment.
+            // * E.G. `use hammer`
+            // * And if we have time you would have a context you want to do previous commands in too. So you could input `nail` after
+            // * to instruct the hammer to be used on the nail.
+            // * 
+            // * */
 
-            //Remembers your last location, if you go out of bounds uses 'ghost' to set your position to your last known actual coordinates.
-            int[] ghost = new int[2];
-            do
-            { 
-                //Sets ghost position to be same as player before player input
-                ghost[0] = player[0];
-                ghost[1] = player[1];
-                Console.WriteLine($"Your position is x {player[0]}, y {player[1]}.");
-                Console.WriteLine("Choose a direction. N/E/S/W\n\n");
-                temp = Console.ReadLine();
-                do
-                {
-                    input = 0;
-                    switch (temp)
-                    {
-                        case "n":
-                        case "N":
-                            player[1]++;
-                            break;
-                        case "s":
-                        case "S":
-                            player[1]--;
-                            break;
-                        case "e":
-                        case "E":
-                            player[0]++;
-                            break;
-                        case "w":
-                        case "W":
-                            player[0]--;
-                            break;
-                        case "x":
-                        case "X":
-                            break;
-                        default:
-                            input = 1;
-                            Console.WriteLine("Invalid Input");
-                            break;
-                    }
-                } while (input == 1);
-                foreach (int value in player)
-                {
-                    //Tests if the player has entered an input that puts them out of bounds, and resets their position to their previous known location
-                    if ((value < 0) || (value > xaxis[xaxis.Length-1]) || (value > yaxis[yaxis.Length-1]))
-                    {
-                        Console.WriteLine("Out of bounds.");
-                        player[0] = ghost[0];
-                        player[1] = ghost[1];
-                    }
-                }
-            } while ((temp != "x")||(temp != "X"));
+
+            //// Main game loop
+            ///*
+            // * Eventually this should maybe read a room text file? E.G. 22.room would be loaded at the start since player is at 
+            // * position[0] = 2;
+            // * position[1] = 2;
+            // * 
+            // * This room file would have information about what can be done in the room and the contextual information about what is in the room
+            // * 
+            // */
+
+            ////Remembers your last location, if you go out of bounds uses 'ghost' to set your position to your last known actual coordinates.
+            //int[] ghost = new int[2];
+            //do
+            //{ 
+            //    //Sets ghost position to be same as player before player input
+            //    ghost[0] = player[0];
+            //    ghost[1] = player[1];
+            //    Console.WriteLine($"Your position is x {player[0]}, y {player[1]}.");
+            //    Console.WriteLine("Choose a direction. N/E/S/W\n\n");
+            //    temp = Console.ReadLine();
+            //    do
+            //    {
+            //        input = 0;
+            //        switch (temp)
+            //        {
+            //            case "n":
+            //            case "N":
+            //                player[1]++;
+            //                break;
+            //            case "s":
+            //            case "S":
+            //                player[1]--;
+            //                break;
+            //            case "e":
+            //            case "E":
+            //                player[0]++;
+            //                break;
+            //            case "w":
+            //            case "W":
+            //                player[0]--;
+            //                break;
+            //            case "x":
+            //            case "X":
+            //                break;
+            //            default:
+            //                input = 1;
+            //                Console.WriteLine("Invalid Input");
+            //                break;
+            //        }
+            //    } while (input == 1);
+            //    foreach (int value in player)
+            //    {
+            //        //Tests if the player has entered an input that puts them out of bounds, and resets their position to their previous known location
+            //        if ((value < 0) || (value > xaxis[xaxis.Length-1]) || (value > yaxis[yaxis.Length-1]))
+            //        {
+            //            Console.WriteLine("Out of bounds.");
+            //            player[0] = ghost[0];
+            //            player[1] = ghost[1];
+            //        }
+            //    }
+            //} while ((temp != "x")||(temp != "X"));
         }
     }
 }
--- room.cs
+++ room.cs
@@ -13,14 +13,45 @@ namespace project_andromeda
          * 
          */
 
-        public Room(int[] player)
+        public static void Read(int[] player)
         {
             int roomx, roomy;
 
             roomx = player[0];
             roomy = player[1];
 
+            string file = @"..\..\..\room\" + roomx + roomy + ".room";
 
+            Console.WriteLine($"Will now attempt to read file {file}");
+            Console.ReadLine();
+
+
+            // Definitely not stolen from the Microsoft example docs
+
+            // Example #2
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            //try
+            //{
+                string[] lines = System.IO.File.ReadAllLines(file);
+            
+
+                // Display the file contents by using a foreach loop.
+                System.Console.WriteLine("Contents of 22.room = ");
+                foreach (string line in lines)
+                {
+                    // Use a tab to indent each line of the file.
+                    Console.WriteLine("\t" + line);
+                }
+
+                Console.ReadLine();
+            
+                //}
+                //catch (System.IO.DirectoryNotFoundException)
+                //{
+                //    Console.WriteLine("Houston we have a problem!" +
+                //        "Unable to read file.");
+                //}
         }
     }
 }
