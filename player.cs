09f7bd98a00d0e5c6a1a94eb234acfae872995d5
Nathan Harris (1000089906) <HARRNG1@student.op.ac.nz>
2021-05-20 16:11:52 +1200

    Barely added inventory feature

---
+++ player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace project_andromeda
+{
+    class Player
+    {
+        public static void die()
+        {
+            Console.WriteLine("You died.");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+        public static void inventory()
+        {
+            ArrayList items = new ArrayList()
+                {
+                    "Chucky Tailors",
+                    "Creel",
+                    "Key",
+                    "Shoes"
+                };
+
+
+            foreach (var val in items)
+                Console.WriteLine(val);
+            Console.ReadLine();
+        }
+    }
+}
+
