d5dd33852812db10fa802d6a061ea8ecb0ec089b
Yousef Altay <83617901+3MKJOE58@users.noreply.github.com>
2021-07-30 10:31:44 +1200

    changed the menu display

--- andromeda.cs
+++ andromeda.cs
@@ -187,6 +187,11 @@ namespace project_andromeda
             do
             {
                 Console.Clear();
+                Console.WriteLine("Maze Advanture Game");
+                Console.WriteLine("========================");
+                Console.WriteLine("1. Start a new game");
+                Console.WriteLine("2. Load the previous game");
+                Console.WriteLine("3. Exit the game");
                 Console.WriteLine("ANDROMEDA\nMain Menu\n\n1. New Game\n2. Load Game\n3. Exit");
                 temp = Console.ReadLine();
                 switch (temp)
